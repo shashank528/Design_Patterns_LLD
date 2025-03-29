4261a27365f7ef2c89149eaa989a6f1c010dbf87
SHASHANK SHEKHAR <shashank.shekharps008@gmail.com>
2025-03-29 13:35:28 +0530

    implement chain of responsibility

---
+++ src/main/java/Behavioural/ChainofResponsibility/ATMDemo.java
@@ -0,0 +1,58 @@
+package Behavioural.ChainofResponsibility;
+abstract class AtmHandler
+{
+    int denomination;
+    AtmHandler nextHandler;
+    public AtmHandler(int denomination)
+    {
+        this.denomination=denomination;
+    }
+    public void setNextHandler(AtmHandler nextHandler)
+    {
+        this.nextHandler=nextHandler;
+    }
+    public void dispense(int amount)
+    {
+        if(amount>denomination)
+        {
+            int nofnote = amount/denomination;
+            System.out.println("no of note of "+denomination+" dispensed ="+nofnote);
+            int remainder = amount%denomination;
+            if(remainder>0)
+            {
+                nextHandler.dispense(remainder);
+            }
+        }
+    }
+}
+class ThousandHanlder extends AtmHandler
+{
+
+    public ThousandHanlder(int denomination) {
+        super(denomination);
+    }
+}
+class FiveHunderedHandler extends AtmHandler{
+
+    public FiveHunderedHandler(int denomination) {
+        super(denomination);
+    }
+}
+class HundredHandler extends AtmHandler
+{
+
+    public HundredHandler(int denomination) {
+        super(denomination);
+    }
+}
+public class ATMDemo {
+    public static void main(String[] args) {
+        ThousandHanlder thousandHanlder = new ThousandHanlder(1000);
+        FiveHunderedHandler fiveHunderedHandler = new FiveHunderedHandler(500);
+        HundredHandler hundredHandler = new HundredHandler(100);
+        thousandHanlder.setNextHandler(fiveHunderedHandler);
+        fiveHunderedHandler.setNextHandler(hundredHandler);
+        thousandHanlder.dispense(3700);
+
+    }
+}
